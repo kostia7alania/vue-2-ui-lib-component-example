a5411635647816747c9ab7726796e38e4435bd6d
Kostia mbp16.2  M1Max 64gb 32c gpu <kostia7alania@gmail.com>
2023-08-03 15:49:20 +0300

    chore: fix deploy + build deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/vue-2-ui-lib-component-example/"
+      ? "/vue-2-ui-lib-component-example/dist/"
       : "/",
 };
